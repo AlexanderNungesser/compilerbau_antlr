fc0626b38ba53a6b4179a9725e89fbb46f68df56
Marie Juenke <marie.juenke@gmx.de>
2024-11-15 16:35:17 +0100

    almost good ast

--- src/main/java/ASTNode.java
+++ src/main/java/ASTNode.java
@@ -2,10 +2,17 @@ import java.util.ArrayList;
 
 public class ASTNode {
     ArrayList children = new ArrayList();
+    String type;
     String value;
 
-    public ASTNode(String value) {
+    public ASTNode(String type, String value) {
+        this.type = type;
         this.value = value;
+    }
+
+    public ASTNode(String type) {
+        this.type = type;
+        this.value = "";
     }
 
     public void addChild(ASTNode child) {
@@ -13,11 +20,18 @@ public class ASTNode {
     }
 
     public void print() {
-        System.out.println(this.value);
-        System.out.print("-->");
-        for (Object child : this.children) {
-            ((ASTNode) child).print();
+        print("", true);
+    }
+
+    private void print(String prefix, boolean isLast) {
+        // Anzeige des aktuellen Knotens
+        System.out.println(prefix + (isLast ? "'__ " : "|-- ") + this.type + (this.value.isEmpty() ? "" : " (" + this.value + ")"));
+
+        // Anzeige der Kinder
+        for (int i = 0; i < children.size(); i++) {
+            ASTNode child = (ASTNode) children.get(i);
+            boolean lastChild = (i == children.size() - 1);
+            child.print(prefix + (isLast ? "    " : "|   "), lastChild);
         }
-        System.out.print("<--");
     }
 }
--- src/main/java/ASTVisitor.java
+++ src/main/java/ASTVisitor.java
@@ -3,19 +3,16 @@ import org.antlr.v4.runtime.tree.RuleNode;
 
 public class ASTVisitor extends MiniCBaseVisitor<ASTNode> {
 
-    private ASTNode checkNode(ParserRuleContext ctx) {
-        ASTNode current;
+    private ASTNode checkNode(ParserRuleContext ctx, ASTNode current) {
         if (ctx.getChildCount() == 0){
             current = new ASTNode(ctx.getText());
         } else if (ctx.getChildCount() == 1) {
             current = visit(ctx.getChild(0));
         } else {
-            current = new ASTNode(ctx.getText());
             for (int i = 0; i < ctx.getChildCount() - 1; i++) {
                 ASTNode child = visit(ctx.getChild(i));
-                if(child != null) {
-                    current.addChild(child);
-                }
+                if(child != null)
+                current.addChild(child);
             }
         }
 
@@ -24,7 +21,7 @@ public class ASTVisitor extends MiniCBaseVisitor<ASTNode> {
 
     @Override
     public ASTNode visitProgram(MiniCParser.ProgramContext ctx) {
-        ASTNode root = new ASTNode(ctx.getText());
+        ASTNode root = new ASTNode("Program");
         for (int i = 0; i < ctx.getChildCount() - 1; i++) {
             ASTNode child = visit(ctx.getChild(i));
             root.addChild(child);
@@ -36,56 +33,91 @@ public class ASTVisitor extends MiniCBaseVisitor<ASTNode> {
 
     @Override
     public ASTNode visitStmt(MiniCParser.StmtContext ctx) {
-        return checkNode(ctx); // Standard-Visitor-Verhalten
+        ASTNode current = new ASTNode(ctx.getText());
+        return checkNode(ctx, current);
     }
 
     @Override
     public ASTNode visitVardecl(MiniCParser.VardeclContext ctx) {
-        return checkNode(ctx);
+        ASTNode current = new ASTNode(ctx.type().getText(), ctx.getText());
+        if (ctx.expr() != null) {
+            current.addChild(visit(ctx.expr())); // Initialisierung
+        }
+        return current;
+
     }
     @Override
     public ASTNode visitAssign(MiniCParser.AssignContext ctx) {
-        return checkNode(ctx);
+        ASTNode current = new ASTNode(ctx.getText());
+        return checkNode(ctx, current);
     }
     @Override
     public ASTNode visitFndecl(MiniCParser.FndeclContext ctx) {
-        return checkNode(ctx);
+        ASTNode current = new ASTNode(ctx.type().getText(), ctx.ID().getText());
+        current.addChild(visit(ctx.params()));
+        current.addChild(visit(ctx.block()));
+        return current;
     }
     @Override
     public ASTNode visitParams(MiniCParser.ParamsContext ctx) {
-        return checkNode(ctx);
+        ASTNode current = new ASTNode(ctx.getText());
+        return checkNode(ctx, current);
     }
     @Override
     public ASTNode visitReturn(MiniCParser.ReturnContext ctx) {
-        return checkNode(ctx);
+        ASTNode current = new ASTNode(ctx.getText());
+        current.addChild(visit(ctx.expr())); // Rückgabewert
+        return current;
     }
     @Override
     public ASTNode visitFncall(MiniCParser.FncallContext ctx) {
-        return checkNode(ctx);
+        ASTNode current = new ASTNode( ctx.ID().getText());
+        current.addChild(visit(ctx.args())); // Argumente
+        return current;
     }
     @Override
     public ASTNode visitArgs(MiniCParser.ArgsContext ctx) {
-        return checkNode(ctx);
+        return checkNode(ctx, new ASTNode(ctx.getText()));
     }
     @Override
     public ASTNode visitBlock(MiniCParser.BlockContext ctx) {
-        return checkNode(ctx);
+        return visit(ctx.getChild(1));
     }
     @Override
     public ASTNode visitWhile(MiniCParser.WhileContext ctx) {
-        return checkNode(ctx);
+        return checkNode(ctx, new ASTNode(ctx.getText()));
     }
     @Override
     public ASTNode visitCond(MiniCParser.CondContext ctx) {
-        return checkNode(ctx);
+        ASTNode current = new ASTNode("if");
+        current.addChild(visit(ctx.expr())); // Bedingung
+        current.addChild(visit(ctx.block(0))); // Then-Block
+        if (ctx.block(1) != null) {
+            current.addChild(visit(ctx.block(1))); // Else-Block
+        }
+        return current;
     }
     @Override
     public ASTNode visitExpr(MiniCParser.ExprContext ctx) {
-        return checkNode(ctx);
+        if (ctx.getChildCount() == 3) { // Binäre Operation
+            ASTNode opNode = new ASTNode(ctx.getChild(1).getText());
+            opNode.addChild(visit(ctx.getChild(0))); // Linker Operand
+            opNode.addChild(visit(ctx.getChild(2))); // Rechter Operand
+            return opNode;
+        } else if (ctx.getChildCount() == 2) { // Unary Operation
+            ASTNode opNode = new ASTNode(ctx.getChild(0).getText());
+            opNode.addChild(visit(ctx.getChild(1))); // Operand
+            return opNode;
+        } else if (ctx.fncall() != null) {
+            return visit(ctx.fncall());
+        } else if (ctx.getChildCount() == 1) { // Terminale
+            return new ASTNode(ctx.getText());
+        }
+        return null;
     }
     @Override
     public ASTNode visitType(MiniCParser.TypeContext ctx) {
-        return checkNode(ctx);
+        return checkNode(ctx, new ASTNode(ctx.getText()));
     }
 
 }
